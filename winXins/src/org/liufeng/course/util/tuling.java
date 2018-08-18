f72737b79be666b114a167fa78ebb5cb512aa95e
xullent <sunxuchu00@163.com>
2018-08-18 23:17:22 +0800

    删除
    删除

--- winXins/src/org/liufeng/course/util/tuling.java
+++
@@ -1,9 +0,0 @@
-package org.liufeng.course.util;
-
-import java.io.IOException;
-
-public class tuling{
-    public static void main(String[] args) throws IOException { 
-    	System.out.println(TulingApiUtil.getTulingResult("ÄãºÃ"));
-    }
-}
